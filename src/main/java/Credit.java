7ec2a615f41e47cb759919692a5057ad3efef004
Olesya <olesya_po@mail.ru>
2024-03-25 20:52:38 +0300

    addFiles

---
+++ src/main/java/Credit.java
@@ -0,0 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.ObjectInputFilter;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Credit {
+    public ObjectInputFilter.Status status;
+}
