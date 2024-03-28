f9730657f129e4200a731062c557fe511942259c
Olesya <olesya_po@mail.ru>
2024-03-28 15:14:48 +0300

    addTest

--- src/test/java/tests/TestPayMyPayment.java
+++ src/test/java/tests/TestPayMyPayment.java
@@ -5,14 +5,15 @@ import dataBase.DBUtils;
 import dataBase.FormPage;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
+
 import java.sql.SQLException;
 
 class TestPayMyPayment {
 
     private FormPage formPage;
 
-   @BeforeEach
-   void setUpPage() {
+    @BeforeEach
+    void setUpPage() {
         formPage = new FormPage();
     }
 
@@ -31,47 +32,48 @@ class TestPayMyPayment {
         SelenideLogger.removeListener("allure");
     }
 
- //   @Test
-   // @DisplayName("Оплата по одобренной карте, обычная покупка, валидные данные")
-  //  void shouldPayByApprovedCard() throws SQLException {
- //       formPage.buyForYourMoney();
-  //      formPage.setCardNumber("4444444444444441");
-  //      formPage.setCardMonth("08");
-  //      formPage.setCardYear("25");
-   //     formPage.setCardOwner("Ivan Petrov");
-   //     formPage.setCardCVV("999");
-   //     formPage.pushСontinueButton();
-  //      formPage.checkMessageSuccess();
-  // }
+    @Test
+    @DisplayName("Payment by approved card, regular purchase, valid details")
+    void shouldPayByApprovedCard() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("25");
+        formPage.setCardOwner("Ivan Petrov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageSuccess();
+    }
 
- //   @Test
-  //  @DisplayName("Оплата по неодобренной карте, обычная покупка с валидными данными")
-  //  void shouldPayByDeclinedCard() throws  SQLException{
-  //     formPage.buyForYourMoney();
-  //     formPage.setCardNumber("4444444444444442");
-   //    formPage.setCardMonth("04");
-   //    formPage.setCardYear("26");
-    //   formPage.setCardOwner("Irina Ivanova");
-   //   formPage.setCardCVV("345");
-   //    formPage.pushСontinueButton();
-   //    formPage.checkMessageSuccess();
-   // }
+    @Test
+    @DisplayName("Payment using an unapproved card, regular purchase with valid data")
+    void shouldPayByDeclinedCard() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("4444444444444442");
+        formPage.setCardMonth("04");
+        formPage.setCardYear("26");
+        formPage.setCardOwner("Irina Ivanova");
+        formPage.setCardCVV("345");
+        formPage.pushСontinueButton();
+        formPage.checkMessageSuccess();
+    }
 
-  //  @Test
-  //    @DisplayName("Оплата по неизвестной карте, обычная покупка с валидными данными")
-  //    void shouldPayBynUnkCard() throws  SQLException {
-   //     formPage.buyForYourMoney();
-   //     formPage.setCardNumber("4444444444444444");
-    //    formPage.setCardMonth("04");
-    //    formPage.setCardYear("26");
-     //   formPage.setCardOwner("Irina Ivanova");
-     //   formPage.setCardCVV("345");
-     //   formPage.pushСontinueButton();
-     //   formPage.checkMessageSuccess();
-    //}
+    @Test
+    @DisplayName("Payment using an unknown card, regular purchase with valid data")
+    void shouldPayBynUnkCard() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("4444444444444444");
+        formPage.setCardMonth("04");
+        formPage.setCardYear("26");
+        formPage.setCardOwner("Irina Ivanova");
+        formPage.setCardCVV("345");
+        formPage.pushСontinueButton();
+        formPage.checkMessageSuccess();
+    }
+
     @Test
     @DisplayName("Payment using a card with an invalid number, regular purchase")
-    void shouldPayInvalidNumberCard() throws  SQLException {
+    void shouldPayInvalidNumberCard() throws SQLException {
         formPage.buyForYourMoney();
         formPage.setCardNumber("444444444444AAAA");
         formPage.setCardMonth("04");
